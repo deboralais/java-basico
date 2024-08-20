5cd7a51efef7a23101cc6bc691218d2fb600e9be
Débora Laís <123045451+deboralais@users.noreply.github.com>
2024-08-20 22:27:50 +0000

    commit aula iago

---
+++ POO/Cursos.java
@@ -0,0 +1,14 @@
+public class Cursos {
+    String modalidade;
+    int periodos;
+    String turno;
+
+    Cursos (String modalidade,
+            int periodos,
+            string turno){
+                 this.modalidade = modalidade;
+                 this.periodos = periodos;
+                 this.turno = turno;
+            }
+    
+}
