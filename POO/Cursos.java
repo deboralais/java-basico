07fd8058877ed8f7ab964c62a9d79e6973d01c20
Débora Laís <123045451+deboralais@users.noreply.github.com>
2024-08-20 22:46:21 +0000

    att commit

--- POO/Cursos.java
+++ POO/Cursos.java
@@ -2,13 +2,16 @@ public class Cursos {
     String modalidade;
     int periodos;
     String turno;
+    String nome;
 
     Cursos (String modalidade,
             int periodos,
-            string turno){
+            string turno,
+            String nome){
                  this.modalidade = modalidade;
                  this.periodos = periodos;
                  this.turno = turno;
+                 this.nome = nome;
             }
     
 }
--- POO/Main.java
+++ POO/Main.java
@@ -1,3 +1,12 @@
 public class Main {
-    
+    public static void main (String[] args) {
+        Cursos curso1 = new Cursos();
+        cursos1.modalidade = "Presencial";
+        curso1.periodos = 4;
+        curso1.turno = "noturno";
+        curso1.nome = "Téc. Enfermagem";  
+
+        Cursos curso2 = new Cursos("Presencial", 2, "noturno", "Téc. Enfermagem")
+        System.out.println(curso2.modalidade + curso2.periodos + curso2.turno + curso2.nome);
+    }
 }
